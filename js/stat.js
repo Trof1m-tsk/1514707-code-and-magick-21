5e1cdf369968ea6f4d7e8f152c267a42a32d04d5
Trofim-tsk <m.trofimov1987@gmail.com>
2020-09-24 22:30:17 +0700

    Добавлена статистика и устранены замечания

--- js/stat.js
+++ js/stat.js
@@ -10,8 +10,10 @@ const BAR_WIDTH = 40;
 const BAR_MAX_HEIGHT = 150;
 const TEXT_HEIGHT = 16;
 const LINE_HEIGHT = TEXT_HEIGHT + GAP;
+const HEADING_GAP = 100;
 const FIRST_BAR_X = CLOUD_X + SPACE_X;
 const FIRST_BAR_Y = CLOUD_Y + LINE_HEIGHT * 3 + GAP + BAR_MAX_HEIGHT;
+const YOUR_STAT_COLOR = `rgba(250, 0, 0, 1)`;
 
 const renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -19,22 +21,18 @@ const renderCloud = function (ctx, x, y, color) {
 };
 
 const getMaxResult = function (times) {
-  let maxResult = times[0];
-
-  for (let i = 1; i < times.length; i++) {
-    if (times[i] > maxResult) {
-      maxResult = times[i];
+  return times.reduce(function (a, b) {
+      return Math.max(a, b);
     }
-  }
-
-  return maxResult;
+  );
 };
 
-const getResult = function (ctx, x, height, name, time) {
-  const myColor = `rgba(250, 0, 0, 1)`;
-  const otherColor = `hsl(240, ` + (Math.random(0) * 100) + `% , 50%)`;
+const getOtherColor = function () {
+  return `hsl(240, ` + (Math.random(0) * 100) + `% , 50%)`;
+};
 
-  ctx.fillStyle = (name === `Вы`) ? myColor : otherColor;
+const renderPlayerStat = function (ctx, x, height, name, time) {
+  ctx.fillStyle = (name === `Вы`) ? YOUR_STAT_COLOR : getOtherColor();
   ctx.fillText(
       Math.round(time),
       FIRST_BAR_X + x,
@@ -59,12 +57,14 @@ window.renderStatistics = function (ctx, names, times) {
 
   ctx.fillStyle = `#000`;
   ctx.font = `16px PT Mono`;
-  ctx.fillText(`Ура вы победили`, CLOUD_X + 100, CLOUD_Y + LINE_HEIGHT);
-  ctx.fillText(`Список результатов:`, CLOUD_X + 100, CLOUD_Y + LINE_HEIGHT * 2);
+  ctx.fillText(`Ура вы победили`, CLOUD_X + HEADING_GAP, CLOUD_Y + LINE_HEIGHT);
+  ctx.fillText(`Список результатов:`, CLOUD_X + HEADING_GAP, CLOUD_Y + LINE_HEIGHT * 2);
 
-  for (let i = 0; i < names.length; i++) {
-    const x = (SPACE_X + BAR_WIDTH) * i;
-    const barHeight = times[i] * BAR_MAX_HEIGHT / getMaxResult(times);
-    getResult(ctx, x, barHeight, names[i], times[i]);
-  }
+  times.forEach(
+    function (playerTime, i) {
+      const x = (SPACE_X + BAR_WIDTH) * i;
+      const barHeight = playerTime * BAR_MAX_HEIGHT / getMaxResult(times);
+      renderPlayerStat(ctx, x, barHeight, names[i], times[i]);
+    }
+  );
 };
